ca535908891e0c59d2bea760c5ca1353a43ccd24
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-13 17:12:34 +0900

    스타일 버튼 추가

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -16,7 +16,7 @@ const GlobalStyle = createGlobalStyle`
         color: #333;
     }
     
-    h1, h2, h3, h4, h5, h6, p, a, div {
+    h1, h2, h3, h4, h5, h6, p, a, div, button {
         margin:0;
         padding:0;
     }
